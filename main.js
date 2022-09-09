29f5d0d67562cec0bb622c2a273ffada340b1d0c
Programador Carioca <Programador.carioca@senacrjedu.local>
2022-09-09 10:59:33 -0300

    Projeto individual

---
+++ main.js
@@ -0,0 +1,32 @@
+/*let nome= "vanessa";
+let diga= "ola";
+
+console.log(diga + nome);
+*/
+//
+
+/*let x ="vanessa";
+let y = 29;
+
+console.log("O usuario de nome: " + x + "tem" + y + "anos");
+
+x = "laysa";
+y = 19;
+ console.log("O usuario de nome: " + x + "tem" + y)
+
+ console.log(typeof y);
+console.log(typeof x);*/
+
+let cliente = "Alcides Garcia";
+let conta = "986798776";
+let saldo = 500;
+
+console.log("cliente: " + cliente);
+console.log("conta: " + conta);
+console.log("saldo: " + saldo);
+
+let deposito = 50.00;
+let SaldoAtual = saldo + deposito;
+
+console.log("Novo deposito na conta" + conta + "saldo atualizado" + SaldoAtual);
+
